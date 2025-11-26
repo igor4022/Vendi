ee8035faa029893a0ae2d58b2730100562bc8658
igor4022 <tic80padraolua@gmail.com>
2025-11-26 20:37:22 -0300

    blz

--- ligar.php
+++ ligar.php
@@ -1,15 +1,15 @@
 <?php
 
-$nome = $_POST['nome']
-$email = $_POST['email']
-$from = $_POST['tic80contacriada@https://igor4022.github.io/Vendi/index.html']
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$from = 'tic80contacriada@https://igor4022.github.io/Vendi/index.html';
 
-$headers = "MIME-Version: 1.1\n"
-$headers .= "Content-type: text/html; charset=utf-8\n"
-$headers .= "From: $nome </$from>\n"
-$headers .= "Return-Path: $from\n"
-$headers .= "Reply-to: $email"
+$headers = "MIME-Version: 1.1\n";
+$headers .= "Content-type: text/html; charset=utf-8\n";
+$headers .= "From: Vendi </$from>\n";
+$headers .= "Return-Path: $from\n";
+$headers .= "Reply-to: $email\n";
 
-mail($email, $headers, $from)
+mail($email, $headers, $from);
 
 ?>
